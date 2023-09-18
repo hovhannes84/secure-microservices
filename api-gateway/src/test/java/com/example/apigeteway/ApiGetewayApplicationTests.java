04233bdc0723528c1e4bae70fbd86668999e2976
Armensargsyan76 <76764242az@gmail.com>
2023-09-18 15:06:38 +0400

    Initial commit

---
+++ api-gateway/src/test/java/com/example/apigeteway/ApiGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
